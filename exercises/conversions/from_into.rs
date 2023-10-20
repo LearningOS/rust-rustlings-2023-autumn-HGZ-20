68fba9b02bb699f094dea3d58b63e5a8483bde63
HGZ-20 <2567430912@qq.com>
2023-10-20 17:31:17 +0000

    update 100

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,46 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person{
+                name: String::from("John"),
+                age: 30,
+            };
+        }
+
+        let parts: Vec<&str> = s.split(",").collect();
+        if parts.len() != 2 {
+            return Person{
+                name: String::from("John"),
+                age: 30,
+            };
+        }
+
+        let name = parts[0];
+        let age_result = parts[1].parse::<usize>();
+
+        if name == "" {
+            return Person{
+                name: String::from("John"),
+                age: 30,
+            };
+        }
+
+        if let Ok(age) = age_result {
+            Person {
+                name: String::from(name),
+                age,
+            }
+        } else {
+            Person{
+                name: String::from("John"),
+                age: 30,
+            }
+        }
     }
 }
 
--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +15,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string() + &input[1..],
     }
 }
 
@@ -24,7 +24,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut new_vec = vec![];
+    for word in words {
+        new_vec.push(capitalize_first(word));
+    }
+    new_vec
 }
 
 // Step 3.
@@ -32,7 +36,12 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let new_vec = capitalize_words_vector(words);
+    let mut new_str = String::new();
+    for word in new_vec {
+        new_str += &word;
+    }
+    new_str
 }
 
 #[cfg(test)]
